3c695546beb1c399bbd559d0d02999bed10f7460
AlexNov12 <nov.alex96@gmail.com>
2024-04-09 13:01:33 +0300

    Initial Commit

---
+++ FoodGolik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodGolik
+//
+//  Created by Александр Новиков on 09.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
